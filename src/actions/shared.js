5e0395cdd8ae206d5e6ce0a369a966366113d52e
mohamed00Sayed <sayedm22529@gmail.com>
2021-09-16 13:57:13 +0200

    personalizing

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -4,7 +4,7 @@ import { receiveTweets } from './tweets'
 import { setAuthedUser } from './authedUser'
 import { showLoading, hideLoading } from 'react-redux-loading'
 
-const AUTHED_ID = 'tylermcginnis'
+const AUTHED_ID = 'm_gaber'
 
 export function handleInitialData () {
   return (dispatch) => {
