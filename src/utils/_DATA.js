358da37329470a45cf68aa72a3579ac41dcdcd93
mohamed00Sayed <sayedm22529@gmail.com>
2021-09-16 14:03:33 +0200

    fixing a problem

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -2,19 +2,19 @@ let users = {
   m_gaber: {
     id: "m_gaber",
     name: "M Gaber",
-    avatarURL: "../avatars/gaber.png",
+    avatarURL: "https://github.com/mohamed00Sayed/reactnd-chirper-app/tree/master/public/avatars/gaber.png",
     tweets: ['8xf0y6ziyjabvozdd253nd', 'hbsc73kzqi75rg7v1e0i6a', '2mb6re13q842wu8n106bhk', '6h5ims9iks66d4m7kqizmv', '3sklxkf9yyfowrf0o1ftbb'],
   },
   m_sayed: {
     id: "m_sayed",
     name: "M Sayed",
-    avatarURL: "../avatars/medo3.png",
+    avatarURL: "https://github.com/mohamed00Sayed/reactnd-chirper-app/tree/master/public/avatars/medo3.png",
     tweets: ['5c9qojr2d1738zlx09afby', 'f4xzgapq7mu783k9t02ghx', 'nnvkjqoevs8t02lzcc0ky', '4pt0px8l0l9g6y69ylivti', 'fap8sdxppna8oabnxljzcv', 'leqp4lzfox7cqvsgdj0e7', '26p5pskqi88i58qmza2gid', 'xi3ca2jcfvpa0i3t4m7ag'],
   },
   m_emad: {
     id: "m_emad",
     name: "M Emad",
-    avatarURL: "../avatars/emad.png",
+    avatarURL: "https://github.com/mohamed00Sayed/reactnd-chirper-app/tree/master/public/avatars/emad.png",
     tweets: ['5w6k1n34dkp1x29cuzn2zn', 'czpa59mg577x1oo45cup0d', 'omdbjl68fxact38hk7ypy6', '3km0v4hf1ps92ajf4z2ytg', 'njv20mq7jsxa6bgsqc97', 'sfljgka8pfddbcer8nuxv', 'r0xu2v1qrxa6ygtvf2rkjw'],
   }
 }
